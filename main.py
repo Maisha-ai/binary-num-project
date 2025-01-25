cc471df338db37b019ef34db1fcd4758a435ee5b
Maisha-ai <maishakarim7@gmail.com>
2025-01-25 20:53:02 +0600

    binary

---
+++ main.py
@@ -0,0 +1,14 @@
+def bide(bistr):
+    
+    if all(char in '01' for char in bistr):
+        
+        decimal_number = int(bistr, 2)
+        return decimal_number
+    else:
+        
+        return print("Invalid binary number. Please ensure the input contains only 0 and 1.")
+
+
+bininput = input("Enter a binary number: ")
+decioutput = bide(bininput)
+print("The decimal of the binary {bininput} is {decioutput}.")
